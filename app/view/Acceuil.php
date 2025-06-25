63b3def71a20b307fdc38d15c1941f611805d4d1
ErwanVigneau <erwan.vigneau@utt.fr>
2025-06-25 18:58:13 +0200

    correction petite erreur

--- app/view/Acceuil.php
+++ app/view/Acceuil.php
@@ -6,7 +6,7 @@
     include 'fragment/fragmentProjetMenu.php';
     
     include 'fragment/fragmentProjetJumbotron.html';
-    echo"<hr><h5>Bienvenu à l'acceuil de notre application ! Utilisez le menu pour accerder aux fonctionnalités !</h5><br>";
+    echo"<hr><h5>Bienvenu à l'acceuil de notre application ! Utilisez le menu pour accéder aux fonctionnalités !</h5><br>";
     ?>
       
   </div>   
